282d3e227f9a2ee8c11fdeab99d646ecd55b1a2f
AjinkyaNagarkarUSC <ajinkyav@usc.edu>
2026-02-20 16:18:19 -0800

    refactor(auth): remove roles from jwt payload

--- backend/server/src/auth/auth.service.ts
+++ backend/server/src/auth/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
     async login(user: User) {
         const roles = (user.roles || []).map(r => r.name);
 
-        const payload = { sub: user.id, roles };
+        const payload = { sub: user.id};
         return {
             access_token: this.jwtService.sign(payload),
         };
